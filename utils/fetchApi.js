7a73e74a56f977651e81f7c21ae87368a105d808
Aniruddh Singh <93520896+a1nn1997@users.noreply.github.com>
2022-02-16 19:43:57 +0530

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,8 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-       'x-rapidapi-key': 'd51128f184mshbf28d3dda792dacp1ca4e0jsn3be14fb40370'
-       /*process.env.NEXT_PUBLIC_RAPID_API_KEY,*/
+       'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY,
     },
   });
     
